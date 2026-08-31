339996d7b35b892f65b711c912643b573d06587b
gPrempeh-lab <George.Prempeh@sainsburys.co.uk>
2026-08-31 16:58:25 +0100

    Add GitHub-only CI/CD demo

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1> CI/CD Demo Works!</h1><p>Deployed from VS Code via GitHub Actions- No Azure needed</p>');
+  res.send('<h1> CI/CD Demo Works!</h1><p>Build in GitHub Actions. so no Azure needed</p>');
 });
 
-app.listen(port, () => {
-  console.log(`Running on port ${port}`);
-}); 
+app.listen(3000);
