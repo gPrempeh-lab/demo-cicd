4bb0e1fee78c0c22ae51d8bb2b390f7f09881660
gPrempeh-lab <George.Prempeh@sainsburys.co.uk>
2026-08-31 16:23:05 +0100

    update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.POR""23000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1> CI/CD Demo Works!</h1><p>Deployed from VS Code via GitHub Actions to Azure</p>');
+  res.send('<h1> CI/CD Demo Works!</h1><p>Deployed from VS Code via GitHub Actions- No Azure needed</p>');
 });
 
 app.listen(port, () => {
